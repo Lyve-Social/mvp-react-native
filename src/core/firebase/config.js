7ec5a6342fc0b8163289ab4bfdf69953fa077096
sshah <shreyansh.shah@liverton.com>
2024-03-02 19:50:53 +1300

    added firebase config, updated package json and added firebase auth to project.

--- src/core/firebase/config.js
+++ src/core/firebase/config.js
@@ -1,6 +1,8 @@
 import fauth from '@react-native-firebase/auth'
 import ffirestore from '@react-native-firebase/firestore'
 import ffunctions from '@react-native-firebase/functions'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 export const db = ffirestore()
 export const auth = fauth
@@ -8,3 +10,16 @@ export const firestore = ffirestore
 export const functions = ffunctions
 export const uploadMediaFunctionURL =
   'https://us-central1-development-69cdc.cloudfunctions.net/uploadMedia'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9ImQCkr3YfLjUaf4qZy9_mdP9cMmDhW0",
+  authDomain: "lyvesocial-android.firebaseapp.com",
+  projectId: "lyvesocial-android",
+  storageBucket: "lyvesocial-android.appspot.com",
+  messagingSenderId: "441693953910",
+  appId: "1:441693953910:web:a97a7f51614f10af1ee457",
+  measurementId: "G-GSWYYXLS4W"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
